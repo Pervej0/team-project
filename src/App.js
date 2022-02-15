8bb0cc75e1df7a0fc73d398eefa9e00998d6b9c1
pervej0 <mdparvez222khan@gmail.com>
2022-02-15 12:26:07 +0600

    react route & file structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Component/Shared/Header/Header";
+import Home from "./Component/Home/Home";
+import About from "./Component/About/About";
+import ContactUs from "./Component/ContactUs/ContactUs";
+import Footer from "./Component/Shared/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-center">Hello world</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
