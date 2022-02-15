0184cbb2aeacf102f3bc64247381a603a0296699
pervej0 <mdparvez222khan@gmail.com>
2022-02-15 11:07:56 +0600

    tailwind setup done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="text-3xl text-center">Hello world</h1>
     </div>
   );
 }
