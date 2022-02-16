03d7b73ac02eb36748993f7a2c878827aa3fb374
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-02-16 23:24:36 +0800

    request form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Component/Shared/Header/Header";
-import Home from "./Component/Home/Home";
 import About from "./Component/About/About";
 import ContactUs from "./Component/ContactUs/ContactUs";
+import Home from "./Component/Home/Home";
+import RequestForBlood from "./Component/RequestForBlood/RequestForBlood";
 import Footer from "./Component/Shared/Footer/Footer";
+import Header from "./Component/Shared/Header/Header";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
+          <Route path="/request" element={<RequestForBlood />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/Component/RequestForBlood/RequestForBlood.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import './RequestForBlood.css';
+
+const RequestForBlood = () => {
+    const { register, formState: { errors }, handleSubmit , reset } = useForm();
+
+    const onSubmit = data => {
+        reset()
+        console.log(data);
+    };
+
+
+    return (
+        <div className="login-register">
+            <div className="login-img">
+                <h1>Request For Blood</h1>
+            </div>
+
+            <div className="Login-form">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <p>Name</p>
+                    <input {...register("name", { required: true })} placeholder="Name" />
+                    {errors.name?.type === 'required' && "Name is required"}
+
+                    <p>Profile</p>
+                    <input {...register("profile", { required: true })} placeholder="profile photo url"/>
+                    {errors.profile && "profile is required"}
+
+                    <p>Phone</p>
+                    <input type="number" {...register("phone", { required: true })} placeholder="Phone" />
+                    {errors.phone?.type === 'required' && "Phone is required"}
+
+                    <p>Blood Group</p>
+                    <input {...register("bloodGroup", { required: true })} placeholder="bloodGroup" />
+                    {errors.bloodGroup?.type === 'required' && "Blood is required"}
+
+                    <p>Bags</p>
+                    <input {...register("bags", { required: true })} placeholder="how many blood bags do you need" />
+                    {errors.bags?.type === 'required' && "Bags is required"}
+
+                    <p>Reason</p>
+                    <input {...register("reason", { required: true })} placeholder="Reason" />
+                    {errors.reason?.type === 'required' && "Reason is required"}
+
+                    <p>Hemoglobin</p>
+                    <input {...register("hemoglobin", { required: true })} placeholder="hemoglobin" />
+                    {errors.hemoglobin?.type === 'required' && "Hemoglobin is required"}
+
+                    <p>Message</p>
+                    <input {...register("message", { required: true })} placeholder="message" />
+                    {errors.message?.type === 'required' && "Message is required"}
+
+                    <p>Knock Me</p>
+                    <input type="time" {...register("knockMe", { required: true })} placeholder="Reason" />
+                    {errors.knockMe?.type === 'required' && "Knock me is required"}
+                    <br />
+                    <br />
+                    <br />
+                    <input type="submit" />
+                   
+                </form>
+            </div>
+            
+        </div>
+    );
+};
+
+export default RequestForBlood;
