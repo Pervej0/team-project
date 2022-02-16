23b7a965acb7ddf10865f1822f21877042ebbf2b
bablu22 <mdbablu22.bablu22@gmail.com>
2022-02-16 19:21:26 +0600

    Home page banner

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,8 @@
-import { data } from "autoprefixer";
 import React, { useEffect, useState } from "react";
 import BloodPost from "./subCompo/BloodPost";
+import Banner from '../Home/Banner/Banner'
+import PostForm from "./PostForm/PostForm";
+
 
 const Home = () => {
   const [postData, setPostData] = useState([]);
@@ -12,12 +14,16 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="lg:w-3/5 md:w-4/5 sm:w-5/6 sm:mx-auto w-full mx-4">
-      <h1 className="text-3xl text-center py-5">Post a blood request</h1>
-      {postData.map((post, index) => (
-        <BloodPost post={post} key={index} />
-      ))}
-    </div>
+    <>
+      <Banner />
+      <PostForm />
+      <div className="lg:w-3/5 md:w-4/5 sm:w-5/6 sm:mx-auto w-full mx-4">
+        <h1 className="text-3xl text-center py-5">Post a blood request</h1>
+        {postData.map((post, index) => (
+          <BloodPost post={post} key={index} />
+        ))}
+      </div>
+    </>
   );
 };
 
