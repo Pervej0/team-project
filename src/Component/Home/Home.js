c089cbf49756763b53bcb852564191ee94da2f18
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2022-02-18 16:50:52 +0800

    sidebar reasign

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -59,20 +59,21 @@ const Home = () => {
           </div>
 
           {/* sidebar */}
-          <div className="bg-gray-50 border dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col h-4/5">
+          <div className="sticky top-0 bg-gray-100 border dark:bg-violet-900 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col h-4/5">
             <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">
-              Customer
+              Top Donner
             </h3>
             <div className="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
               <div className="flex flex-col justify-start items-start flex-shrink-0">
                 <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
                   <img
-                    src="https://i.ibb.co/5TSg7f6/Rectangle-18.png"
+                  className="rounded-full"
+                    src="https://lh3.googleusercontent.com/a/AATXAJzv0OxW890sHSqrc3bLvrUM0ILY20-2Yc6yUzNh=s96-c"
                     alt="avatar"
                   />
                   <div className="flex justify-start items-start flex-col space-y-2">
                     <p className="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">
-                      David Kent
+                    Bablu Mia
                     </p>
                     <p className="text-sm dark:text-gray-300 leading-5 text-gray-600">
                       10 Previous Orders
@@ -102,7 +103,7 @@ const Home = () => {
                     />
                   </svg>
                   <p className="cursor-pointer text-sm leading-5 ">
-                    david89@gmail.com
+                  anytingforgame@gmail.com
                   </p>
                 </div>
               </div>
@@ -113,14 +114,14 @@ const Home = () => {
                       Shipping Address
                     </p>
                     <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                      180 North King Street, Northhampton MA 1060
+                      1200 Farmgate, Dhaka 
                     </p>
                   </div>
                   <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
                     <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">
                       Billing Address
                     </p>
-                    <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
+                    <p className="mb-6 w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
                       180 North King Street, Northhampton MA 1060
                     </p>
                   </div>
